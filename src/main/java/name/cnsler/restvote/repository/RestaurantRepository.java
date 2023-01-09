0fdf05ed982c3b512b56046ea6e9b05a9cb76f39
cnsler <cnsler@gmail.com>
2023-01-09 15:04:49 +0200

    add Repository and RestController for Restaurant

---
+++ src/main/java/name/cnsler/restvote/repository/RestaurantRepository.java
@@ -0,0 +1,8 @@
+package name.cnsler.restvote.repository;
+
+import name.cnsler.restvote.model.Restaurant;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepository extends BaseRepository<Restaurant> {
+}
---
+++ src/main/java/name/cnsler/restvote/web/restaurant/RestaurantController.java
@@ -0,0 +1,61 @@
+package name.cnsler.restvote.web.restaurant;
+
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import name.cnsler.restvote.model.Restaurant;
+import name.cnsler.restvote.repository.RestaurantRepository;
+import name.cnsler.restvote.util.validation.ValidationUtil;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = RestaurantController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+@AllArgsConstructor
+@Slf4j
+public class RestaurantController {
+    static final String REST_URL = "/api/restaurants";
+    private final RestaurantRepository restaurantRepository;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Restaurant> createWithLocation(@Valid @RequestBody Restaurant restaurant) {
+        log.info("crate {}", restaurant);
+        ValidationUtil.checkNew(restaurant);
+        Restaurant created = restaurantRepository.save(restaurant);
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL + "/{id}")
+                .buildAndExpand(created.getId())
+                .toUri();
+        return ResponseEntity.created(uriOfNewResource).body(created);
+    }
+
+    @GetMapping("/{id}")
+    public Restaurant get(@PathVariable int id) {
+        log.info("get {}", id);
+        return restaurantRepository.getExisted(id);
+    }
+
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void update(@Valid @RequestBody Restaurant restaurant, @PathVariable int id) {
+        log.info("update {} with id={}", restaurant, id);
+        ValidationUtil.assureIdConsistent(restaurant, id);
+        restaurantRepository.save(restaurant);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id) {
+        log.info("delete {}", id);
+        restaurantRepository.deleteExisted(id);
+    }
+
+    @GetMapping
+    public List<Restaurant> getAll() {
+        log.info("getAll");
+        return restaurantRepository.findAll();
+    }
+}
