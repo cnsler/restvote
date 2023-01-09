2231f56857de8e5fbfe9b13b50769741445fac4d
cnsler <cnsler@gmail.com>
2023-01-09 14:52:13 +0200

    fill DB table RESTAURANT

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,8 @@ INSERT INTO USER_ROLE (ROLE, USER_ID)
 VALUES ('ADMIN', 1),
        ('USER', 1),
        ('USER', 2);
+
+INSERT INTO RESTAURANT (TITLE)
+VALUES ('Огонек'),
+       ('Рыбак'),
+       ('Кавказ')
