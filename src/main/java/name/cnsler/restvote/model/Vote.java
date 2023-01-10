d1258bd899d0c6833a79952c35f9864bcc4622dd
cnsler <cnsler@gmail.com>
2023-01-10 23:02:28 +0200

    add Vote entity

---
+++ src/main/java/name/cnsler/restvote/model/Vote.java
@@ -0,0 +1,37 @@
+package name.cnsler.restvote.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "vote_date"}, name = "vote_unique_user_date_idx")})
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Vote extends BaseEntity {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @JsonIgnore
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    private Restaurant restaurant;
+
+    @Column(name = "vote_date", nullable = false)
+    private LocalDate voteDate;
+
+    @Override
+    public String toString() {
+        return "Vote{" +
+                "id=" + id +
+                ", userId=" + user.getId() +
+                ", restaurantId=" + restaurant.getId() +
+                ", voteDate=" + voteDate +
+                '}';
+    }
+}
