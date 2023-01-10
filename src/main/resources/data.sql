d3279bf1482366008344ff1bbfc339938093d5b0
cnsler <cnsler@gmail.com>
2023-01-10 23:03:10 +0200

    fill DB table VOTE

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,24 @@
 INSERT INTO USERS (NAME, EMAIL, PASSWORD)
 VALUES ('Admin', 'a@a', '{noop}adminPass'),
-       ('User', 'u@u', '{noop}userPass');
+       ('User', 'u@u', '{noop}userPass'),
+       ('New User', 'n@u', '{noop}newPass');
 
 INSERT INTO USER_ROLE (ROLE, USER_ID)
 VALUES ('ADMIN', 1),
        ('USER', 1),
-       ('USER', 2);
+       ('USER', 2),
+       ('USER', 3);
 
 INSERT INTO RESTAURANT (TITLE)
 VALUES ('Огонек'),
        ('Рыбак'),
-       ('Кавказ')
+       ('Кавказ');
+
+INSERT INTO VOTE (VOTE_DATE, RESTAURANT_ID, USER_ID)
+VALUES (now() - 2, 1, 1),
+       (now() - 2, 1, 2),
+       (now() - 2, 2, 3),
+       (now() - 1, 3, 1),
+       (now() - 1, 3, 2),
+       (now() - 1, 3, 3),
+       (now(), 1, 3)
