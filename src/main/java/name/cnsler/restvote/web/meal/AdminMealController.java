76998070add7ed36d8e2bd4f9fbbf86a8ad3362f
cnsler <cnsler@gmail.com>
2023-01-13 22:48:48 +0200

    refactor AdminMealController
    - delete getAll on date
    - create with restaurant id reference
    - fix restaurant parameter type in delete method

--- src/main/java/name/cnsler/restvote/web/meal/AdminMealController.java
+++ src/main/java/name/cnsler/restvote/web/meal/AdminMealController.java
@@ -1,6 +1,5 @@
 package name.cnsler.restvote.web.meal;
 
-import jakarta.annotation.Nullable;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,7 +8,6 @@ import name.cnsler.restvote.model.Meal;
 import name.cnsler.restvote.model.Restaurant;
 import name.cnsler.restvote.repository.MealRepository;
 import name.cnsler.restvote.repository.RestaurantRepository;
-import name.cnsler.restvote.util.validation.ValidationUtil;
 import org.springframework.data.domain.Example;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -17,9 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.time.LocalDate;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 @RestController
@@ -33,16 +29,8 @@ public class AdminMealController {
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Meal> createWithLocation(@PathVariable int restaurantId, @Valid @RequestBody Meal meal) {
-        log.info("restaurantId = {}", restaurantId);
-        log.info("start request to DB");
-        log.info("restaurantRepository.count() = {}", restaurantRepository.count());
-        log.info("end request to DB");
-        //TODO it's check restaurant id exists? Maybe BaseRepository.getExisted()?
-        if (restaurantId < 1 || restaurantId > restaurantRepository.count()) {
-            throw new IllegalRequestDataException("Restaurant with id=" + restaurantId + " not found");
-        }
         meal.setRestaurant(restaurantRepository.getReferenceById(restaurantId));
-//        meal.setRestaurant(restaurantRepository.getExisted(restaurantId));
+        //TODO catch Exception
         Meal created = mealRepository.save(meal);
         log.info("{} created", created);
         URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
@@ -78,40 +66,20 @@ public class AdminMealController {
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable String restaurantId, @PathVariable int id) {
-        //TODO check restaurant id exists
-        //TODO check meal belong to restaurant
+    public void delete(@PathVariable int restaurantId, @PathVariable int id) {
+        //TODO check restaurant id exists? Or change path /api/meals/{id}
+        //TODO check meal belong to restaurant?
         log.info("delete meal with id={}", id);
         mealRepository.deleteExisted(id);
     }
 
     @GetMapping
     public List<Meal> getAll(@PathVariable int restaurantId) {
-        log.info("1");
+        //TODO optimize SQL query: without Restaurant title (r1_0.title=?)
         Meal mealProbe = new Meal();
-        log.info("2");
         mealProbe.setRestaurant(restaurantRepository.getExisted(restaurantId));
-        log.info("3");
-        Example<Meal> mealForRestaurant = Example.of(mealProbe);
-        log.info("4");
-        List<Meal> meals = mealRepository.findAll(mealForRestaurant);
-        log.info("5");
-        log.info("getAll meals {} for restaurant id={}", meals, restaurantId);
-        return meals;
-    }
-
-    @GetMapping("/on-date")
-    public List<Meal> getAllOnDate(@PathVariable int restaurantId, @Nullable LocalDate date) {
-        LocalDate mealDate = Objects.requireNonNullElse(date, LocalDate.now());
-        log.info("1");
-        Meal mealProbe = new Meal();
-        log.info("2");
-        mealProbe.setRestaurant(restaurantRepository.getExisted(restaurantId));
-        log.info("3");
-        mealProbe.setMealDate(mealDate);
-        log.info("4");
         List<Meal> meals = mealRepository.findAll(Example.of(mealProbe));
-        log.info("getAll meals {} for restaurant id={} on date={}", meals, restaurantId, mealDate);
+        log.info("getAll meals {} for restaurant id={}", meals, restaurantId);
         return meals;
     }
 }
